3250f32c8994b727dfb67ec28826977c48efaf96
Scott Olson <scott@solson.me>
2015-12-08 19:58:05 -0600

    Add solutions for day 4.

---
+++ src/bin/day04.rs
@@ -0,0 +1,32 @@
+extern crate aoc;
+extern crate md5;
+
+fn main() {
+    const KEY: &'static [u8] = b"bgvyzdsv";
+
+    let mut i: u64 = 1;
+    let mut vec = Vec::from(KEY);
+
+    let mut five_zeroes = None;
+    let mut six_zeroes = None;
+
+    while five_zeroes.is_none() || six_zeroes.is_none() {
+        vec.extend(i.to_string().bytes());
+
+        let digest = md5::compute(&vec);
+
+        if five_zeroes.is_none() && digest[0..2] == [0; 2] && digest[2] <= 0x0F {
+            five_zeroes = Some(i);
+        }
+
+        if six_zeroes.is_none() && digest[0..3] == [0; 3] {
+            six_zeroes = Some(i);
+        }
+
+        vec.truncate(KEY.len());
+        i += 1;
+    }
+
+    println!("The five-zeroes answer is {}.", five_zeroes.unwrap());
+    println!("The six-zeroes answer is {}.", six_zeroes.unwrap());
+}
